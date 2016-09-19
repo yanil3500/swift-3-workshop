e045571af914c7e8e9284d3b56e927dbc6f29f3b
awallraff <adam.wallraff@gmail.com>
2016-09-19 10:23:25 -0700

    Working on lab-1

--- CFS3ToDoList/Swift3Workshop.playground/Contents.swift
+++ CFS3ToDoList/Swift3Workshop.playground/Contents.swift
@@ -3,3 +3,54 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Numeric Type
+
+
+
+//String
+
+
+
+//Type Inference
+
+
+
+//Tuple
+
+
+
+//Set
+
+
+
+//Array
+
+
+
+//Dictionary
+
+
+
+//For-In Loop
+
+
+
+//While Loop
+
+
+
+//Repeat While
+
+
+//If Statement
+
+
+
+//Switch Statement
+
+
+
+//Optionals
+
+
